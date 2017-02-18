e96e6a46c54011cba988495bf0837e049569e191
Janworks <jan.wichelmann@hotmail.de>
2017-02-18 18:22:53 +0100

    Initial commit

---
+++ ScenarioLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScenarioLibrary
+{
+    public class Class1
+    {
+    }
+}
